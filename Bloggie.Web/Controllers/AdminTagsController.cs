77766b4e1b2c63b074fee4ff19f68bb2344db854
Brian Donnoe <dunow33@hotmail.com>
2023-11-03 23:52:28 -0400

    initial commit

---
+++ Bloggie.Web/Controllers/AdminTagsController.cs
@@ -0,0 +1,39 @@
+﻿using Bloggie.Web.Models.ViewModels;
+using Bloggie.Web.Models.Domain;
+using Bloggie.Web.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Bloggie.Web.Controllers
+{
+    public class AdminTagsController : Controller
+    {
+        private BloggieDbContext bloggieDbContext;
+
+        public AdminTagsController(BloggieDbContext bloggieDbContext)
+        {
+            this.bloggieDbContext = bloggieDbContext;
+        }
+
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Add(AddTagRequest addTagRequest)
+        {
+            var tag = new Tag
+            {
+                Name = addTagRequest.Name,
+                DisplayName = addTagRequest.DisplayName
+            };
+
+            bloggieDbContext.Tags.Add(tag);
+            bloggieDbContext.SaveChanges();
+
+            return View("Add");
+        }
+    }
+}

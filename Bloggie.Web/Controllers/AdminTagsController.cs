c7f97a9714e8b2cd33d7a0ae02c372dc54d479aa
Brian Donnoe <dunow33@hotmail.com>
2023-11-06 17:36:12 -0500

    making more progress

--- Bloggie.Web/Controllers/AdminTagsController.cs
+++ Bloggie.Web/Controllers/AdminTagsController.cs
@@ -9,10 +9,11 @@ namespace Bloggie.Web.Controllers;
 public class AdminTagsController : Controller
 {
     private readonly ITagInterface tagRepository;
+    private readonly BloggieDbContext bloggieDbContext;
 
-    public AdminTagsController(ITagInterface tagRepository)
+    public AdminTagsController(BloggieDbContext bloggieDbContext)
     {
-        this.tagRepository = tagRepository;
+        this.bloggieDbContext = bloggieDbContext;
     }
 
 
@@ -32,17 +33,79 @@ public class AdminTagsController : Controller
             DisplayName = addTagRequest.DisplayName
         };
 
-		//bloggieDbContext.Tags.Add(tag);
-		//bloggieDbContext.SaveChanges();
-		await tagRepository.AddAsync(tag);
+		bloggieDbContext.Tags.Add(tag);
+		bloggieDbContext.SaveChanges();
+		//await tagRepository.AddAsync(tag);
 
 		return RedirectToAction("List");
     }
 
     [HttpGet]
-    public async Task<IActionResult> List()
+    public IActionResult List()
     {
-        var tags = await tagRepository.GetAllAsync();
+        //var tags = await tagRepository.GetAllAsync();
+        var tags = bloggieDbContext.Tags.ToList();
+
         return View(tags);
+    }
+
+    [HttpGet]
+    public IActionResult Edit(Guid id)
+    {
+        var tag = bloggieDbContext.Tags.FirstOrDefault(x => x.Id == id);
+        //var tags = await tagRepository.GetAllAsync();
+        if(tag!= null)
+        {
+            var editTagRequest = new EditTagRequest
+            {
+                Id = tag.Id,
+                Name = tag.Name,
+                DisplayName = tag.DisplayName
+            };
+
+            return View(editTagRequest);
+        }
+
+        return View(null);
+    }
+
+    [HttpPost]
+    public IActionResult Edit(EditTagRequest editTagRequest)
+    {
+        var tag = new Tag
+        {
+            Id = editTagRequest.Id,
+            Name = editTagRequest.Name,
+            DisplayName = editTagRequest.DisplayName
+        };
+
+        var existingTag = bloggieDbContext.Tags.Find(tag.Id);
+
+        if(existingTag != null)
+        {
+            existingTag.Name = tag.Name;
+            existingTag.DisplayName = tag.DisplayName;
+
+            bloggieDbContext.SaveChanges();
+            return RedirectToAction("Edit", new { id = editTagRequest.Id });
+        }
+
+        return RedirectToAction("Edit", new { id = editTagRequest.Id });
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Delete(EditTagRequest editTagRequest)
+    {
+        var tag = bloggieDbContext.Tags.Find(editTagRequest.Id);
+
+        if (tag != null)
+        {
+            bloggieDbContext.Tags.Remove(tag);
+            bloggieDbContext.SaveChanges();
+
+            return RedirectToAction("List");
+        }
+
+        return RedirectToAction("Edit", new { id = editTagRequest.Id });
     }
 }

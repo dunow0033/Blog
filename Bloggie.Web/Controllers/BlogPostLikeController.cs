11a89d332383dc1b4d48dc5da87cef7990e6dd5e
Brian Donnoe <dunow33@hotmail.com>
2023-11-10 17:59:37 -0500

    making more progress

---
+++ Bloggie.Web/Controllers/BlogPostLikeController.cs
@@ -0,0 +1,17 @@
+﻿using Bloggie.Web.Models.ViewModels;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Bloggie.Web.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class BlogPostLikeController : ControllerBase
+{
+    [HttpPost]
+    [Route("Add")]
+    public async Task<IActionResult> AddLike([FromBody] AddLikeRequest addLikeRequest)
+    {
+
+    }
+}
--- Bloggie.Web/Repositories/IBlogPostLikeRepository.cs
+++ Bloggie.Web/Repositories/IBlogPostLikeRepository.cs
@@ -1,6 +1,10 @@
-﻿namespace Bloggie.Web.Repositories;
+﻿using Bloggie.Web.Models.Domain;
+
+namespace Bloggie.Web.Repositories;
 
 public interface IBlogPostLikeRepository
 {
 	Task<int> GetTotalLikes(Guid blogPostId);
+
+	Task<BlogPostLike> AddLikeForBlog(BlogPostLike blogPostLike);
 }
